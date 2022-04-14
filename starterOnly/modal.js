d8f02e6530db0f2e6a257bc67d8df2b7b2fdbedf
Benjamin Siret <benjaminsiret@outlook.fr>
2022-04-14 14:07:24 +0200

    add emailValidation function

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,42 +13,63 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalCloseBtn = document.querySelector(".close");
 const firstNameInput = document.querySelector("#first");
+const lastNameInput = document.querySelector("#last");
+const emailInput = document.querySelector("#email");
 
 // Regex
-const firstNameRegex = /^[A-Za-zÀ-ÿ-]{2,}$/i;
+const nameRegex = /^[A-Za-zÀ-ÿ-]{2,}$/i;
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
 //***********FUNCTIONS************
+
 function firstNameValidation() {
-  if (!firstNameInput.value.match(firstNameRegex)) {
+  if (!firstNameInput.value.match(nameRegex)) {
     firstNameInput.style.border = "solid 3px red";
+    alert("Veuillez entrer au moins 2 caractères valides pour ce champ");
     return false;
   } else {
     firstNameInput.style.border = "none";
     return true;
   }
 }
-// launch modal form
+
+function lastNameValidation() {
+  if (!lastNameInput.value.match(nameRegex)) {
+    lastNameInput.style.border = "solid 3px red";
+    alert("Veuillez entrer au moins 2 caractères valides pour ce champ");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function emailValidation() {
+  if (!emailInput.value.match(emailRegex)) {
+    emailInput.style.border = "solid 3px red";
+    alert("Veuillez entrer une adresse email valide");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// launch modal function
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// close modal form
+// close modal function
 function closeModal() {
   modalbg.style.display = "none";
 }
 
-// validate form
+// validate form function
 function validate() {
   // (1) Lier les labels aux entrées dans le HTML en utilisant les attributs "for" et "id" dans le code existant. Corriger le code HTML quand nécessaire.
   // (2) Utiliser du JavaScript pur (pas de jQuery) pour terminer le formulaire :
   //     Le formulaire doit être valide quand l'utilisateur clique sur "Submit"
   //     Les données doivent être saisies correctement :
   //     (1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
-  if (!firstNameValidation()) {
-    alert("formulaire non valide");
-  } else {
-    alert("formulaire ok :)");
-  }
   //     (2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
   //     (3) L'adresse électronique est valide.
   //     (4) Pour le nombre de concours, une valeur numérique est saisie.
@@ -67,3 +88,9 @@ modalCloseBtn.addEventListener("click", closeModal);
 
 // validate first name
 firstNameInput.addEventListener("change", firstNameValidation);
+
+// validate last name
+lastNameInput.addEventListener("change", lastNameValidation);
+
+// validate email
+emailInput.addEventListener("change", emailValidation);
