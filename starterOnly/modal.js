45109a094e2d1285d0d2fb94b3c640907b1daef7
Benjamin Siret <benjaminsiret@outlook.fr>
2022-04-14 13:11:32 +0200

    create firstNameValidation function, test in validate function

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,18 +7,26 @@ function editNav() {
   }
 }
 
-// DOM Elements
+//*************DOM Elements****************
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalCloseBtn = document.querySelector(".close");
+const firstNameInput = document.querySelector("#first");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// Regex
+const firstNameRegex = /^[A-Za-zÀ-ÿ-]{2,}$/i;
 
-// close modal event
-modalCloseBtn.addEventListener("click", closeModal);
-
+//***********FUNCTIONS************
+function firstNameValidation() {
+  if (!firstNameInput.value.match(firstNameRegex)) {
+    firstNameInput.style.border = "solid 3px red";
+    return false;
+  } else {
+    firstNameInput.style.border = "none";
+    return true;
+  }
+}
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
@@ -36,6 +44,11 @@ function validate() {
   //     Le formulaire doit être valide quand l'utilisateur clique sur "Submit"
   //     Les données doivent être saisies correctement :
   //     (1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
+  if (!firstNameValidation()) {
+    alert("formulaire non valide");
+  } else {
+    alert("formulaire ok :)");
+  }
   //     (2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
   //     (3) L'adresse électronique est valide.
   //     (4) Pour le nombre de concours, une valeur numérique est saisie.
@@ -43,3 +56,14 @@ function validate() {
   //     (6) La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée.
   //     Conserver les données du formulaire (ne pas effacer le formulaire) lorsqu'il ne passe pas la validation.
 }
+
+//********** MODAL EVENTS**************
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+modalCloseBtn.addEventListener("click", closeModal);
+
+// validate first name
+firstNameInput.addEventListener("change", firstNameValidation);
